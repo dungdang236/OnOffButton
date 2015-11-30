dcbbcfd51e4f55f355167b42ded2791c363ca6dd
student <student@techmasters-imac-2.local>
2015-11-30 13:24:14 -0800

    Initial Commit

---
+++ OnOffButton/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OnOffButton
+//
+//  Created by student on 30/11/2015.
+//  Copyright © 2015 dungdang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
